e37ff6b4a4a2c1c0188539afff5ba049675c03e4
Or Israeli <or.israeli@duda.co>
2022-10-03 16:35:58 +0300

    bdx bug crawler

---
+++ crawler.ts
@@ -0,0 +1,32 @@
+const nFetch = require("node-fetch");
+
+const INTERVAL = parseInt(process.env.interval) || 3600000;
+const SEARCHED_STRING = process.env.searchedString || "atlanta";
+setInterval(crawlPage, INTERVAL);
+
+async function crawlPage() {
+  const res = await nFetch("https://www.smithdouglas.com/sitemap.xml", {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
+    },
+  });
+  const resInText = await res.text();
+  if (resInText.toLowerCase().includes(SEARCHED_STRING)) {
+    await nFetch(
+      "https://hooks.slack.com/services/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",
+      {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          text: `🦝searched string detected:🦝\n ${SEARCHED_STRING}`,
+        }),
+      }
+    );
+  }
+}
+crawlPage();
